88360d89eb1cda1fea09e51d17eae50700224c8c
Carlos Balaguer <balaguer.88@gmail.com>
2018-09-17 14:40:04 +0200

    commit

--- src/app/components/App.vue
+++ src/app/components/App.vue
@@ -3,6 +3,10 @@
         <div>
           <nav class="navbar navbar-dark bg-primary">
             <a href="/" class="navbar-brand">Dialapp HUGCS</a>
+            <div id="btnlogout" class="row mb-8">
+              <button @click="logout" class="btn btn-outline-danger"> Cerrar Sesion </button>
+            <h4>{{ msg }}</h4>
+        </div>
           </nav>
         </div>
         <router-view></router-view>
